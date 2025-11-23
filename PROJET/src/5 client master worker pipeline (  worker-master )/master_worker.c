391fba98991e951341e037b64d1d37d63ca25ae7
lvadegre <lvadegre>
2025-11-23 02:19:20 +0100

    ajout des etapes de realisation

---
+++ PROJET/src/5 client master worker pipeline (  worker-master )/master_worker.c
@@ -0,0 +1,12 @@
+#if defined HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "master_worker.h"
+
+// fonctions éventuelles internes au fichier
+
+// fonctions éventuelles proposées dans le .h
